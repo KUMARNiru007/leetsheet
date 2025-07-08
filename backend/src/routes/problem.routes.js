0221aaee202b40d631210ced228b8d25a93e5b42
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-07-08 21:09:12 +0530

    updated create problem controller and routes

--- backend/src/routes/problem.routes.js
+++ backend/src/routes/problem.routes.js
@@ -1,17 +1,17 @@
 import express from 'express'
 import { authMiddleware, checkAdmin } from '../middleware/auth.middleware.js';
-import { createProblem, deleteProblem, getAllProblemsSolvedByUser, getProblemById, updatedProblem } from '../controllers/problem.controller.js';
+import { createProblem, deleteProblem, getAllProblems, getAllProblemsSolvedByUser, getProblemById, updateProblem } from '../controllers/problem.controller.js';
 
 
 const problemRoutes = express.Router();
 
 problemRoutes.post("/create-problem", authMiddleware ,checkAdmin , createProblem )
 
-problemRoutes.post("/get-all-problems", authMiddleware , getAllProblemsSolvedByUser)
+problemRoutes.get("/get-all-problems", authMiddleware , getAllProblems)
 
-problemRoutes.post("/get-problem/:id",authMiddleware,getProblemById);
+problemRoutes.get("/get-problem/:id",authMiddleware,getProblemById);
 
-problemRoutes.get("/update-problem/:id", authMiddleware , checkAdmin , updatedProblem )
+problemRoutes.put("/update-problem/:id", authMiddleware , checkAdmin , updateProblem )
 
 problemRoutes.delete("/delete-problem/:id" , authMiddleware , checkAdmin , deleteProblem)
 
