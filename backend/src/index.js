b2e78b16ead8ee9138fb14199454f81a67c60f8d
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-07-11 22:31:05 +0530

    exceuteCode controller complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import cors from "cors";
 
 import authRoutes from "../src/routes/auth.routes.js";
 import problemRoutes from "./routes/problem.routes.js";
+import executionRoute from "./routes/executeCode.routes.js";
 
 
 dotenv.config();
@@ -34,6 +35,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/auth" , authRoutes);
 app.use("/api/v1/problems", problemRoutes);
+app.use("/api/v1/execute-code", executionRoute);
 
 app.listen(process.env.PORT, () => {
     console.log("Server is running on port 8080 ");
