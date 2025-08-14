e0f34092b326cec861c7dfb86b1f6003c4cf7101
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-08-14 16:15:52 +0530

    work started in and made rough landing page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,29 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "../index.css";
 
-
-export default function Navbar() {}
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-logo">
+            LeetLab
+          </Link>
+        </div>
+        
+        <div className="navbar-menu">
+          <Link to="/" className="navbar-link">Home</Link>
+          <Link to="/problems" className="navbar-link">Problems</Link>
+          <Link to="/sheets" className="navbar-link">Sheet</Link>
+          <Link to="/About" className="navbar-link">About</Link>
+        </div>
+        
+        <div className="navbar-auth">
+          <Link to="/login" className="navbar-link">Login</Link>
+          <Link to="/signup" className="navbar-link signup-btn">Sign Up</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
