6c2a09e0ac4af9135180b6d093e9152558f745a9
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-08-31 15:54:25 +0530

    fix error in dep

--- backend/src/index.js
+++ backend/src/index.js
@@ -53,7 +53,7 @@ app.use(
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
       sameSite: isProduction ? 'none' : 'lax',
-      domain: isProduction ? '.codeleap.in' : undefined,
+      domain: isProduction ? '.leetsheet.in' : undefined,
     },
   }),
 );
